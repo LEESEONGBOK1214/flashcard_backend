c122568ccc16f9ac5b279c58c64729d5862400e1
leeseongbok <dltjdqhr1000@gmail.com>
2022-10-14 13:46:14 +0900

    temp save

--- src/main/java/com/teosprint/flashcard/config/SecurityConfig.java
+++ src/main/java/com/teosprint/flashcard/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.teosprint.flashcard.config;
 
 
-import com.teosprint.flashcard.config.jwt.JwsTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/teosprint/flashcard/config/jwt/JwsTokenProvider.java
+++ src/main/java/com/teosprint/flashcard/config/jwt/JwsTokenProvider.java
@@ -22,7 +22,7 @@ import java.util.stream.Collectors;
 public class JwsTokenProvider {
 
     private static final String AUTHORITIES_KEY = "role";
-    private static final String BEARER_TYPE = "bearer";
+    private static final String BEARER_TYPE = "Bearer";
     private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
 
